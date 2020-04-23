225072037bda88d258ff2e9908fcc28d87478cdb
Yoset Cozco Mauri <yodetcozco@gmail.com>
2020-04-23 16:49:13 -0500

    fib.java added

--- fib.java
+++ fib.java
@@ -1,6 +1,8 @@
+
 import java.util.*;
-class fib{
-	public static void main(String args[]{
+
+public class fib{
+	public static void main(String args[]){
 	Scanner sc = new Scanner(System.in);
 	int n =  sc.nextInt();
 	System.out.println(fib(n));
